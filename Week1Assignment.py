895cd392f37dc627f1db51d736ba06d6186332af
MDZULFIKAR ALI <mdzulfikarali@MDZULFIKARs-MacBook-Pro.local>
2020-06-28 22:21:51 -0400

    adding a python script for week1 assignmenr

---
+++ Week1Assignment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
